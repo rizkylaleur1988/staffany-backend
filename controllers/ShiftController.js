d4bac7339f8b049e9f5b4c16ee21be9f0a6d0188
Mochamad Rizki <rizkylaleur1988@gmail.com>
2021-02-01 14:32:52 +0700

    updated

--- controllers/ShiftController.js
+++ controllers/ShiftController.js
@@ -1,5 +1,5 @@
-const { json } = require("body-parser");
 let Shift = require("./../models/Shift");
+let moment = require("moment");
 
 exports.index = (req, res) => {
   Shift.get((err, data) => {
@@ -13,11 +13,22 @@ exports.index = (req, res) => {
 
 exports.store = (req, res) => {
   const { name, shift_date, start_time, end_time } = req.body;
-  const shift = new Shift({ name, shift_date, start_time, end_time });
-  shift.save((err, data) => {
-    if (err) res.json({ status: false, message: err });
-    res.json({ status: true, data, message: "New data created" });
-  });
+
+  const startTime = moment().set("hours", start_time.split(":")[0]).set("minutes", start_time.split(":")[1]);
+  const endTime = moment().set("hours", end_time.split(":")[0]).set("minutes", end_time.split(":")[1]);
+  const duration = moment.duration(endTime.diff(startTime)).asMinutes();
+
+  if (parseInt(duration) < 0) {
+    res.json({ status: false, message: "Your start time greater than end time" });
+  } else if (duration === 0) {
+    res.json({ status: false, message: "Your start time same with end time" });
+  } else {
+    const shift = new Shift({ name, shift_date, start_time, end_time });
+    shift.save((err, data) => {
+      if (err) res.json({ status: false, message: err });
+      res.json({ status: true, data, message: "New data created" });
+    });
+  }
 };
 
 exports.show = (req, res) => {
@@ -34,20 +45,30 @@ exports.show = (req, res) => {
 exports.update = (req, res) => {
   const { id } = req.params;
   const { name, shift_date, start_time, end_time } = req.body;
-  Shift.findById(id)
-    .then((shift) => {
-      shift.name = name;
-      shift.shift_date = shift_date;
-      shift.start_time = start_time;
-      shift.end_time = end_time;
-      return shift.save();
-    })
-    .then((data) => {
-      res.json({ status: true, data, message: "Data updated" });
-    })
-    .catch((err) => {
-      res.json({ status: false, message: err });
-    });
+
+  const startTime = moment().set("hours", start_time.split(":")[0]).set("minutes", start_time.split(":")[1]);
+  const endTime = moment().set("hours", end_time.split(":")[0]).set("minutes", end_time.split(":")[1]);
+  const duration = moment.duration(endTime.diff(startTime)).asMinutes();
+  if (parseInt(duration) < 0) {
+    res.json({ status: false, message: "Your start time greater than end time" });
+  } else if (duration === 0) {
+    res.json({ status: false, message: "Your start time same with end time" });
+  } else {
+    Shift.findById(id)
+      .then((shift) => {
+        shift.name = name;
+        shift.shift_date = shift_date;
+        shift.start_time = start_time;
+        shift.end_time = end_time;
+        return shift.save();
+      })
+      .then((data) => {
+        res.json({ status: true, data, message: "Data updated" });
+      })
+      .catch((err) => {
+        res.json({ status: false, message: err });
+      });
+  }
 };
 
 exports.delete = (req, res) => {
